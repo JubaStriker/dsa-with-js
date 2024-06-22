a906aa5e9719955364f1c6ec710cf03e01de15c5
JubaStriker <jubastriker@gmail.com>
2024-06-22 23:57:59 +0600

    Logic implemented

--- insertionSort.js
+++ insertionSort.js
@@ -1,5 +1,15 @@
 function insertionSort(arr) {
-    // I will do it today anyhow
-    // for keeping the streak
+    for (let i = 1; i < arr.length; i++) {
+        let numberToInsert = arr[i]
+        let j = i - 1;
+        while (j >= 0 && arr[j] > numberToInsert) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j] = numberToInsert;
+    }
+
     return arr
 }
+
+const arr = [8, 20, -2, 4, -6]
