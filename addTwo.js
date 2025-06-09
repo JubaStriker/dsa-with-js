373aff6fffd513c2cbbfb4619d3b93a42fb650df
JubaStriker <jubastriker@gmail.com>
2025-06-10 00:42:56 +0600

    Linked list understood

--- addTwo.js
+++ addTwo.js
@@ -1,25 +1,46 @@
-var twoSum = function (nums, target) {
-    for (let i = 0; i <= nums.length; i++) {
-        for (let j = i + 1; j <= nums.length - 1; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j]
-            }
-        }
+class ListNode {
+    constructor(val = 0, next = null) {
+        this.val = val;
+        this.next = next;
     }
-};
-
-
-
-function twoSumOptimal(nums, target) {
-    let map = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        let complement = target - nums[i];
-        if (map.has(complement)) {
-            return [map.get(complement), i];
-        }
-        map.set(nums[i], i);
-    }
-    return [];
 }
 
-console.log(twoSumOptimal([2, 7, 11, 15], 9));
+function addTwoNumbers(l1, l2) {
+    let dummyHead = new ListNode(0);
+    let current = dummyHead;
+    let carry = 0;
+
+    while (l1 || l2 || carry) {
+        const x = l1 ? l1.val : 0;
+        const y = l2 ? l2?.val : 0;
+
+        const sum = x + y + carry;
+        carry = Math.floor(sum / 10);
+
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null
+    }
+
+    return dummyHead.next;
+}
+
+function arrayToLinkedList(arr) {
+    let dummyHead = new ListNode(0);
+    let current = dummyHead;
+
+    for (let val of arr) {
+        current.next = new ListNode(val);
+        current = current.next;
+    }
+
+    return dummyHead.next;
+}
+
+// Modified test case
+const l1 = arrayToLinkedList([2, 4, 3]);
+const l2 = arrayToLinkedList([5, 6, 4]);
+
+console.log(addTwoNumbers(l1, l2));
