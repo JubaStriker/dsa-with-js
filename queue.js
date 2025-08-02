e1ca91d5433a67a04c03aa8fbb2326fd0122c51f
JubaStriker <jubastriker@gmail.com>
2025-08-02 21:53:10 +0600

    Queue structure

---
+++ queue.js
@@ -0,0 +1,32 @@
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+
+    enqueue(element) {
+        this.items.push(element);
+    }
+
+    dequeue() {
+        return this.items.shift();
+    }
+
+    peek() {
+        if (!this.items.isEmpty()) {
+            return this.items[0];
+        }
+        return null;
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    size() {
+        return this.items.length;
+    }
+
+    print() {
+        console.log(this.items.join(" <- "));
+    }
+}
