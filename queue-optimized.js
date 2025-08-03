9701fa4753aa7b19214e1e75b039302d8e723960
JubaStriker <jubastriker@gmail.com>
2025-08-03 23:31:19 +0600

    Queue optimized with objects

---
+++ queue-optimized.js
@@ -0,0 +1,46 @@
+class Queue {
+    constructor() {
+        this.items = {};
+        this.rear = 0;
+        this.front = 0;
+    }
+
+    enqueue(element) {
+        this.items[this.rear] = element;
+        this.rear++;
+    }
+
+    dequeue() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        const item = this.items[this.front];
+        delete this.items[this.front];
+        this.front++;
+        return item;
+    }
+
+    isEmpty() {
+        return this.near - this.front === 0;
+    }
+
+    peek() {
+        this.items[this.front] || null;
+    }
+
+    size() {
+        return this.rear - this.front;
+    }
+
+    print() {
+        console.log(this.items);
+    }
+}
+
+const queue = new Queue();
+console.log(queue.isEmpty());
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+queue.print();
+console.log(queue.size());      
