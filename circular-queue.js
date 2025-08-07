02edc85ad22690630aaf9e299012de8cc31a3a05
JubaStriker <jubastriker@gmail.com>
2025-08-07 23:38:26 +0600

    Circular queue updated

--- circular-queue.js
+++ circular-queue.js
@@ -3,5 +3,15 @@ class CircularQueue {
         this.items = new Array(capacity);
         this.capacity = capacity;
         this.currentLength = 0;
+        this.rear = -1;
+        this.front = -1
+    }
+
+    isFull() {
+        return this.currentLength === this.capacity;
+    }
+
+    isEmpty() {
+        return this.currentLength === 0;
     }
 }
