ed6cce4bd93e20b88c3105ac0e5718479d791c83
JubaStriker <jubastriker@gmail.com>
2025-08-17 20:09:23 +0600

    Max sub array

---
+++ maxSubArray.js
@@ -0,0 +1,19 @@
+function maxSubArray(arr) {
+
+    let currentSum = arr[0];
+    let maxSum = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        let num = arr[i];
+        currentSum = Math.max(num, currentSum + num);
+        maxSum = Math.max(maxSum, currentSum)
+    }
+    return maxSum;
+}
+
+
+const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+const maxSum = maxSubArray(arr);
+console.log(maxSum);
+
+// Using Kadane’s algorithm
