96d3f6ebaa4f7e242eb3b7476f0f8083d53a366a
JubaStriker <jubastriker@gmail.com>
2025-08-22 23:13:48 +0600

    Lined list class initiated

--- linedList.js
+++ linedList.js
@@ -4,3 +4,18 @@ class Node {
         this.next = null;
     }
 }
+
+class LinedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+
+    getSize() {
+        return this.size;
+    }
+}
