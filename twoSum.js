5ec5c8a1d78e15710ad72afb9d92c46c65f5449a
JubaStriker <jubair@transfi.com>
2026-04-25 16:36:04 +0600

    Add filter function and example usage in filterElements.js; implement longestSubstring function in longestSubstring.js

--- twoSum.js
+++ twoSum.js
@@ -9,17 +9,31 @@ var twoSum = function (nums, target) {
 };
 
 
-
 function twoSumOptimal(nums, target) {
-    const numMap = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if (numMap.has(complement)) {
-            return [numMap.get(complement), i];
-        }
-        numMap.set(nums[i], i);
+    const map = new Map();
 
+    for (let i = 0; i <= nums.length; i++) {
+        const compliment = target - nums[i];
+
+        if (map.has(compliment)) {
+            return [map.get(compliment), i]
+        }
+
+        map.set(nums[i], i)
     }
 }
 
-console.log(twoSumOptimal([2, 7, 11, 15], 26));
+function twoSumPointers(nums, target) {
+    let L = 0, R = nums.length - 1
+
+    while (L < R) {
+        let sum = nums[L] + nums[R];
+        if (sum === target) return [L, R]
+        else if (sum < target) L++;
+        else R--;
+    }
+}
+
+
+//console.log(twoSumOptimal([2, 7, 11, 15], 9));
+console.log(twoSumPointers([2, 3, 7, 11, 15], 9))
